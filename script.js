fdb7581097d6e015fcb8f42344adc73106c860d8
PerryChuChu <perry93104@gmail.com>
2026-01-17 14:49:47 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const liffId = "先不要改";
+const sheetUrl = "先不要改";
+
+liff.init({ liffId }).then(() => {
+  if (!liff.isLoggedIn()) liff.login();
+});
+
+function send(answer) {
+  liff.getProfile().then(p => {
+    fetch(sheetUrl, {
+      method: "POST",
+      body: JSON.stringify({
+        userId: p.userId,
+        name: p.displayName,
+        answer
+      })
+    });
+    alert("已送出，請看大螢幕！");
+  });
+}
